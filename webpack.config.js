49e61ce16525a733cddd404d875bd55ad444c98b
Eric Daams <eric@164a.com>
2018-02-19 14:06:54 +0930

    Added donors icon back in. Added react-select.

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,10 @@ module.exports = {
         path: path.join( __dirname, 'assets/js' ),
         filename: 'charitable-blocks.js'
     },
-<<<<<<< HEAD
     externals: {
 		react: 'React',
 		'react-dom': 'ReactDOM',
 	},
-=======
->>>>>>> Added Charitable_Assets class as a cleaner way to organize script & style registration & enqueuement. Added selectWoo script in prep of having multi-select fields in blocks. Minified blocks script via Webpack.
     stats: {
         colors: false,
         modules: true,
